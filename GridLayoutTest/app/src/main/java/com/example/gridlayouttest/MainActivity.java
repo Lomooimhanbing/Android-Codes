c5148a8d0db2f66334bd26940dd6ba2dea21ddf7
Lomoo <Lomoo@outlook.kr>
2022-09-09 15:04:41 +0800

    Version 0.0.1

--- GridLayoutTest/app/src/main/java/com/example/gridlayouttest/MainActivity.java
+++ GridLayoutTest/app/src/main/java/com/example/gridlayouttest/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.gridlayouttest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.Button;
+import android.widget.GridLayout;
 
 public class MainActivity extends AppCompatActivity {
+    GridLayout gridLayout;
+    String[] chars = new String[]{
+            "7", "8", "9", "/",
+            "4", "5", "6", "*",
+            "1", "2", "3", "-",
+            ".", "0", "=", "+",
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        gridLayout = findViewById(R.id.root);
+        for (int i = 0; i < chars.length; i++) {
+            Button bn = new Button(this);
+            bn.setText(chars[i]);
+            bn.setTextSize(20);
+            GridLayout.Spec rowSpec = GridLayout.spec(i / 4 + 2);
+            GridLayout.Spec columnSpec = GridLayout.spec(i % 4);
+            GridLayout.LayoutParams params = new GridLayout.LayoutParams(rowSpec, columnSpec);
+            params.setGravity(Gravity.FILL);
+            gridLayout.addView(bn, params);
+        }
     }
 }
